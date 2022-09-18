39dbfbc095957aab0bb05e19332f53c59be4f745
paolabris <pamodiazbr@ittepic.edu.mx>
2022-09-18 01:56:30 -0600

    navegation completo

--- app/src/main/java/com/example/ladm_u1_practica2/MainActivity.kt
+++ app/src/main/java/com/example/ladm_u1_practica2/MainActivity.kt
@@ -26,10 +26,13 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(binding.appBarMain.toolbar)
 
+
         binding.appBarMain.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            Snackbar.make(view, "¿Desea salir?", Snackbar.LENGTH_LONG)
+                .setAction("SI", { finish() }).show()
         }
+
+
         val drawerLayout: DrawerLayout = binding.drawerLayout
         val navView: NavigationView = binding.navView
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -37,7 +40,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.eliminarFragment
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
